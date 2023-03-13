0e1be9282e2d7d8ea1343507225938bc03e1f4b9
Matus Martiska <matus.martiska@gmail.com>
2023-03-13 20:35:23 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import { AddNewUser } from "./components/addnewuser";
+import { useState, useEffect } from "react";
+import AddNewUser from "./components/AddNewUser";
 import Header from "./components/Header";
-import USERLIST from "./components/UserList";
+import UserList from "./components/UserList";
 import "./styles.css";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((raw_json) => {
+        raw_json.json().then((data) => {
+          setUsers(data);
+        });
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }, []);
+
   return (
     <div className="App">
-      <div>
-        <Header name={"User foo"} />
-      </div>
-      <div>
-        <AddNewUser />
-      </div>
-      <div>
-        <USERLIST />
-      </div>
+      {users.length !== 0 ? (
+        <>
+          <div>
+            <Header users={users} />
+          </div>
+          <div>
+            <AddNewUser setUsers={setUsers} />
+          </div>
+          <div>
+            <UserList users={users} />
+          </div>
+        </>
+      ) : (
+        <div>Loading data...</div>
+      )}
     </div>
   );
 }
